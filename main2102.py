5c63af789660007465ac5caad138995a6cab1006
Djemchik <jemil1.2004@mail.ru>
2023-02-21 21:05:10 +0300

    DUPDATE

--- main2102.py
+++ main2102.py
@@ -2,6 +2,7 @@ import ipaddress
 import requests
 from tqdm import tqdm  # прогресс-бар
 
+
 def calculate_allowed_ips(ip_addresses):
     allowed_ips = set()
     for ip_address in ip_addresses:
@@ -15,56 +16,30 @@ def calculate_allowed_ips(ip_addresses):
     return allowed_ips
 
 
-# def calculate_allowed_ips(ip_list):
-#     """
-#     Рассчитывает маски подсетей для списка IP-адресов в формате CIDR.
+def main():
+    # делаем GET-запрос к API и получаем данные в формате JSON
+    url = "https://reestr.rublacklist.net/api/v3/ips/"
+    response = requests.get(url)
+    data = response.json()
 
-#     :param ip_list: список IP-адресов в формате CIDR
-#     :return: список масок подсетей в формате CIDR
-#     """
-#     # преобразуем каждый IP-адрес в объект ipaddress.IPv4Network
-#     networks = [ipaddress.IPv4Network(ip) for ip in ip_list]
+    # выбираем только IPv4-адреса и объединяем их, если это возможно
+    ipv4_addresses = []
+    for i, address in enumerate(tqdm(data, desc="Processing IP addresses")):
+        if ":" not in address:
+            ipv4_addresses.append(address)
+        # выводим прогресс выполнения цикла каждые 5000 итераций
+        if i > 0 and i % 5000 == 0:
+            tqdm.write(f"Processed {i} IPv4 addresses")
 
-#     # сортируем список сетей по адресу
-#     sorted_networks = sorted(networks)
+    # рассчитываем маски подсетей
+    allowed_ips = calculate_allowed_ips(ipv4_addresses)
 
-#     # объединяем перекрывающиеся сети
-#     merged_networks = []
-#     current_network = sorted_networks[0]
-#     for network in sorted_networks[1:]:
-#         if current_network.overlaps(network):
-#             current_network = current_network.supernet_of(network)
-#         else:
-#             merged_networks.append(current_network)
-#             current_network = network
-#         # выводим прогресс выполнения цикла каждые 1000 итераций
-#         if len(merged_networks) % 1000 == 0:
-#             tqdm.write(f"Processed {len(merged_networks)} networks")
+    # выводим результат
+    result = "Allowed IPs = " + ", ".join(allowed_ips)
+    print("\033[92m" + result + "\033[0m")  # выводим сообщение в зеленом цвете
 
-#     merged_networks.append(current_network)
+    print("\033[1m" + "\033[91m" + "УСПЕХ" + "\033[0m") # выводим сообщение "УСПЕХ" в разноцветных буквах
 
-#     # преобразуем каждую маску подсети в формат "адрес/маска подсети"
-#     allowed_ips = [f"{str(network.network_address)}/{str(network.netmask)}" for network in merged_networks]
 
-#     return allowed_ips
-
-# делаем GET-запрос к API и получаем данные в формате JSON
-url = "https://reestr.rublacklist.net/api/v3/ips/"
-response = requests.get(url)
-data = response.json()
-
-# выбираем только IPv4-адреса и объединяем их, если это возможно
-ipv4_addresses = []
-for address in tqdm(data, desc="Processing IP addresses"):
-    if ":" not in address:
-        ipv4_addresses.append(address)
-    # выводим прогресс выполнения цикла каждые 5000 итераций
-    if len(ipv4_addresses) % 5000 == 0:
-        tqdm.write(f"Processed {len(ipv4_addresses)} IPv4 addresses")
-
-# рассчитываем маски подсетей
-allowed_ips = calculate_allowed_ips(ipv4_addresses)
-
-# выводим результат
-result = "Allowed IPs = " + ", ".join(allowed_ips)
-print("\033[92m" + result + "\033[0m")  # выводим сообщение в зеленом цвете
+if __name__ == "__main__":
+    main()
